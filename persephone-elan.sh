adb13d6ea9798a967a0059cf2bf15c504823c834
Christopher Cox <christopher.cox@carleton.ca>
2019-10-09 15:10:55 -0400

    Minor fixes to shell script

--- persephone-elan.sh
+++ persephone-elan.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-# Set the default encoding to something Unicode-friendly before starting the
-# actual recognizer.  Otherwise, the default is ASCII and Persephone refuses
-# to run.
+#
+# Set a number of environmental variables and locale-related settings needed
+# for Persephone to run as expected before calling the recognizer itself.
+#
+# It seems that recognizer processes invoked by ELAN don't inherit any regular
+# environmental variables (like PATH), which makes it difficult to track down
+# where both Python and ffmpeg(1) might be.  These same processes also have
+# their locale set to C.  This implies a default ASCII file encoding, which
+# causes Persephone to refuse to run (since it seems to assume a more Unicode-
+# friendly view of the world somewhere in its code).
 
-# Edit the following three lines as needed.
+# **
+# ** Edit the following two lines to point to the Python 3 executable and the
+# ** directory in which 'ffmpeg' is found on this computer.
+# **
 export PYTHON3="/Library/Frameworks/Python.framework/Versions/3.6/bin/python3"
 export FFMPEG_DIR="/Users/chris/Unix"
-export LC_ALL="en_CA.UTF-8"
 
+export LC_ALL="en_US.UTF-8"
 export PYTHONIOENCODING="utf-8"
 export PATH="$PATH:$FFMPEG_DIR"
 
